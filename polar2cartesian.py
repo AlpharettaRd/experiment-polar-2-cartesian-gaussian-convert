959de63e50427d4f567eb6bf44a8709968f41b32
Edelweiss <60952333+AlpharettaRd@users.noreply.github.com>
2022-04-03 20:30:58 -0400

    Update polar2cartesian.py

--- polar2cartesian.py
+++ polar2cartesian.py
@@ -3,13 +3,12 @@ import random
 import numpy as np
 
 if __name__ == '__main__':
-    distance = 8.0
+    distance = 10.0
     bearing = 0
     distance_noise = 0.05
     bearing_noise  = 0.03
-    number = 8
+    number = 6
     for n in range(number):
-        bearing += math.pi / number
         print(f'--------distance: {distance}, distance noise: {distance_noise}, bearing: {bearing}, bearing noise: {bearing_noise}--------')
         data_both = []
         data_dist = []
@@ -33,15 +32,18 @@ if __name__ == '__main__':
         exp_both_sqrt = math.sqrt(exp_dist_std ** 2 + exp_bear_std ** 2)
         distance_with_noise = distance + random.gauss(0, distance_noise)
         bearing_with_noise = bearing + random.gauss(0, bearing_noise)
-        cal_dist_std = distance_noise * math.cos(bearing)
-        cal_bear_std = distance * bearing_noise * (math.sin(bearing))
+        cal_dist_std = abs(distance_noise * math.cos(bearing))
+
+        cal_bear_std = distance * math.sqrt((math.sin(bearing) ** 2)  * (bearing_noise ** 2) + ((math.cos(bearing) ** 2) * (bearing_noise ** 4) / 2.0))
+
         cal_both_std = math.sqrt(cal_dist_std**2 + cal_bear_std**2)
-        diff_bear_std = cal_bear_std - exp_bear_std
-        diff_dist_std = cal_dist_std - exp_dist_std
+        diff_bear_std = abs(cal_bear_std) - abs(exp_bear_std)
+        diff_dist_std = abs(cal_dist_std) - abs(exp_dist_std)
         diff_both_std = cal_both_std - exp_both_std
 
-        print(f'exp_dist_std: {exp_dist_std}, cal_dist_std: {cal_dist_std}, diff: {cal_dist_std - exp_dist_std}')
-        print(f'exp_bear_std: {exp_bear_std}, cal_bear_std: {cal_bear_std}, diff: {cal_bear_std - exp_bear_std}')
-        print(f'exp_both_sqrt: {exp_both_sqrt}, diff: {exp_both_sqrt - exp_both_std}')
+        print(f'exp_dist_std: {exp_dist_std}, cal_dist_std: {cal_dist_std}, diff: {diff_dist_std}')
+        print(f'exp_bear_std: {exp_bear_std}, cal_bear_std: {cal_bear_std}, diff: {diff_bear_std}')
+        print(f'exp_both_sqrt: {exp_both_sqrt}, diff: {diff_both_std}')
         print(f'exp_both_std: {exp_both_std}, cal_both_std: {cal_both_std}, diff: {cal_both_std - exp_both_std}')
         print('\n')
+        bearing += math.pi / number
